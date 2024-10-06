3c45187baf0865010661a6a6f063fac9c4113797
thomazf <thomaz.fneves@gmail.com>
2024-10-05 23:33:56 -0300

    remove unnecessary files for base branch

--- src/main/java/br/com/possoler/api/api_posso_ler/api/utils/rest_connection/RestConnection.java
+++ src/main/java/br/com/possoler/api/api_posso_ler/api/utils/rest_connection/RestConnection.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Component
 public class RestConnection {
 
@@ -25,5 +28,9 @@ public class RestConnection {
     protected ResponseEntity<String> get(String URL) {
         entity = new HttpEntity<>(null);
         return restTemplate.exchange(URL, HttpMethod.GET, entity, String.class);
+    }
+
+    protected String encodeURI(String uri) {
+        return URLEncoder.encode(uri, StandardCharsets.UTF_8);
     }
 }
--- src/main/java/br/com/possoler/api/api_posso_ler/site/interfaces/FactoryHTMLElements.java
+++
@@ -1,8 +0,0 @@
-package br.com.possoler.api.api_posso_ler.site.interfaces;
-
-import org.springframework.ui.Model;
-
-public interface FactoryHTMLElements {
-
-    void factoryElement(String idioma, Model model);
-}
